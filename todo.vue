425e4eacc09480ee46730673d73e042da9a0d44f
raymondjan <raymond.jan@auo.com>
2023-12-05 15:34:47 +0800

    Update todo.vue

--- todo.vue
+++ todo.vue
@@ -1,6 +1,5 @@
 <script setup>
-
-import { ref} from 'vue'
+import { ref } from 'vue'
 
 let id = 0
 
@@ -8,80 +7,51 @@ const title = "Todo list"
 
 const newTodo = ref('')
 
- 
-
 const todos = ref([
-
-  { id: id++, text: 'TEST', done: true },
-
-  { id: id++, text: 'TEST2', done: true },
-
-  { id: id++, text: 'TEST3', done: false }
+  { id: id++, text: 'todo1', done: true },
+  { id: id++, text: 'todo2', done: true },
+  { id: id++, text: 'todo3', done: true }
 
 ])
 
 function addTodo() {
-
   todos.value.push({ id: id++, text: newTodo.value, done: false })
-
   newTodo.value = ''
-
 }
-
- 
-
- 
 
 function removeTodo(todo) {
-
   todos.value = todos.value.filter((t) => t !== todo)
-
 }
-
- 
-
 </script>
 
- 
-
 <template>
-
-  <h1 class="title">{{ title }}</h1>
-
-  <form @submit.prevent="addTodo">
-
-    <input v-model="newTodo" placeholder="Do something...">
-
-    <button>Add Todo</button>
-
-  </form>
-
-  <ul>
-
-    <li v-for="todo in todos" :key="todo.id">
-
-      <input type="checkbox" v-model="todo.done" >
-
-      <span :class="{ done: todo.done }">{{ todo.text }}</span>
-
-      <button @click="removeTodo(todo)">X</button>
-
-    </li>
-
-  </ul>
-
- 
-
+  <div>
+    <h1 class="title">{{ title }}</h1>
+    <p>for study todo app</p>
+    <form @submit.prevent="addTodo">
+      <input v-model="newTodo" placeholder="做點重要的事情..." style="font-size: 26px; width: 60%;" />
+      <button style="font-size: 26px; width: 10%;">新增</button>
+    </form>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        <input type="checkbox" v-model="todo.done">
+        <span :class="{ done: todo.done }" style="font-size: 26px;">{{ todo.text }}</span>
+        <button @click="removeTodo(todo)" style="font-size: 26px;">X</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
- 
-
 <style>
-
-.done {
-
-  text-decoration: line-through;
-
+.title {
+  font-size: 48px; 
 }
 
+form, ul {
+  font-size: 26px; 
+}
+
+.done {
+  text-decoration: line-through;
+}
 </style>
